8c202f8ba2b0fd8d60ea9e17f1c3b1fe8cfb1272
Somnath Mishra <somnathmishra100dbi@gmail.com>
2026-07-16 00:35:17 +0530

    feat: add clerk orgs

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import { UserButton } from "@clerk/nextjs"
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 
 export default function Page() {
-  return <UserButton />
+  return (
+    <main className="flex min-h-svh flex-col items-start gap-3 p-6">
+      <UserButton />
+      <OrganizationSwitcher />
+    </main>
+  )
 }
